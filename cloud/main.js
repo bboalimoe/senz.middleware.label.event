36766fbf9640be17fe304ef324cdecee6be346d7
jiaying.lu <lujiaying93@foxmail.com>
2015-05-12 17:34:56 +0800

    update 更新函数组织结构；进一步完成getLabelEvents() 的功能

--- cloud/main.js
+++ cloud/main.js
@@ -1,42 +1,66 @@
 /// <reference path="../typings/node/node.d.ts"/>
 // Use AV.Cloud.define to define as many cloud functions as you want.
 
-AV.Cloud.define('countNonEmpty', function (request, response) {
-  var query = new AV.Query('Event');
-  query.notEqualTo('label', null);
-  query.find({
-    success: function (results) {
-      response.success(results.length);
-    },
-    error: function () {
-      response.error('Event lookup failed');
-    }
+/// requires
+var myRequest = require('request');
+
+// LabelEvent Object definition
+function LabelEvent(label, createdAt, updatedAt) {
+  this.label = label;
+  this.createdAt = createdAt;
+  this.updatedAt = updatedAt;
+  this.sensorList = undefined;
+  this.micList = undefined;
+  this.locationList = undefined;
+  this.senzList = undefined;
+  this.addSensorList = function (sensorList) {
+    this.sensorList = sensorList;
+  };
+  this.addMicList = function (micList) {
+    this.micList = micList;
+  };
+  this.addLocationList = function (locationList) {
+    this.locationList = locationList;
+  };
+  this.addSenzList = function (senzList) {
+    this.senzList = senzList;
+  };
+};
+
+/// local functions 
+function genResultArray(arr) {
+  var outArr = [];
+  arr.forEach(function (elem) {
+    outArr.push({ 'timestamp': elem.get('timestamp') });
   });
-});
+  return outArr;
+};
+
+function retrieveAPI(locationList, micList, sensorList) {
+  var postData = {
+    "filter": 120000,
+    "timelines": {
+      "location": locationList,
+      "sound": micList,
+      "motion": sensorList,
+    },
+    "primary_key": "location",
+  };
+  //console.log(postData);
+  return new AV.Promise(function (reslove, reject) {
+    myRequest.post({ url: 'http://120.27.30.239:9123/', json: postData }, function (err, httpResponse, body) {
+      if (err) {
+        console.error('retriveAPI failed:', err);
+        reject([]);
+      }
+      //console.log('retriveAPI result:', body['result']);
+      reslove(body['result']);
+    });
+  });
+};
 
 
 AV.Cloud.define('getLabelEvents', function (request, response) {
-  //var myRequest = require('request');
-  // LabelEvent Object definition
-  function LabelEvent(label, createdAt, updatedAt) {
-    this.label = label;
-    this.createdAt = createdAt;
-    this.updatedAt = updatedAt;
-    this.sensorList = undefined;
-    this.micList = undefined;
-    this.poiList = undefined;
-    this.addSensorList = function (sensorList) {
-      this.sensorList = sensorList;
-    }
-    this.addMicList = function (micList) {
-      this.micList = micList;
-    }
-    this.addPoiList = function (poiList) {
-      this.poiList = poiList;
-    }
-  }
-  
-  // start process log
   var labelEvents = new Array(); // for API return
   
   // find specify tag
@@ -66,39 +90,33 @@ AV.Cloud.define('getLabelEvents', function (request, response) {
         locationQuery.find()
         ).then(function (micList, sensorList, locationList) {
         console.log("In when's then");
-        function getTimestampArray(arr) {
-          var outArr = [];
-          arr.forEach(function (elem) {
-            outArr.push(elem.get('timestamp'));
-          });
-          return outArr;
-        }
-        
-        labelEvent.addMicList(getTimestampArray(micList));
-        labelEvent.addSensorList(getTimestampArray(sensorList));
-        labelEvent.addPoiList(getTimestampArray(locationList));
+        labelEvent.addMicList(genResultArray(micList));
+        labelEvent.addSensorList(genResultArray(sensorList));
+        labelEvent.addLocationList(genResultArray(locationList));
         labelEvents.push(labelEvent);
         return AV.Promise.as('Done');
       }));
-      
-      //promises.push(micQuery.find().then(function (result) {
-      //  var micList = new Array();
-      //  result.forEach(function (elem) {
-      //    micList.push(elem.get('timestamp'));
-      //  });
-      //  labelEvent.addMicList(micList);
-      //  return AV.Promise.as('Done');
-      //}));
-      
     });
     console.log('before return');
     return AV.Promise.when(promises);
   }, function (error) {
       response.error('getLabelEvents error when query Event');
       console.error(error);
-    }).then(function (result) {
-    console.log('** labelEvents: **')
-    console.log(labelEvents.length);
+    }).then(function () {
+    // use senz.middleware.log.rawsenz
+    var promises = [];
+    labelEvents.forEach(function (labelEvent) {
+      promises.push(
+          retrieveAPI(labelEvent.locationList, labelEvent.micList, labelEvent.sensorList).then(function (senzList) {
+            console.log('addSenzList');
+            labelEvent.addSenzList(senzList);
+          })
+        );
+    });
+    return AV.Promise.when(promises);
+  }).then(function () {
+    console.log('** labelEvents: **');
+    console.log(labelEvents);
     response.success(labelEvents);
   });
 });
@@ -121,28 +139,5 @@ AV.Cloud.define('promiseTest', function (request, response) {
   ]).then(function (result1) {
     console.log(result1);
     console.log('---------------');
-  });
-});
-
-
-AV.Cloud.define('tryAPI', function (request, response) {
-  var myRequest = require('request');
-  var postData = {
-    "filter": 120000,
-    "timelines": {
-      "location": [{ "timestamp": 1431329016771 }, { "timestamp": 1431331962757 }],
-      "motion": [{ "timestamp": 1431329016771 }, { "timestamp": 1431329870846 }],
-      "sound": [{ "timestamp": 1431331962757 }, { "timestamp": 1431323000593 }],
-    },
-    "primary_key": "location",
-  };
-  myRequest.post({ url: 'http://120.27.30.239:9123/', json: postData }, function (err, httpResponse, body) {
-    if (err) {
-      return console.error('upload failed:', err);
-    }
-    console.log('The content of result is:\n' + JSON.stringify(body, null, 4));
-    var result = new Object({ 'result': body['result'] });
-    console.log(result);
-    console.log('Upload successful!  Server responded with:', body['result']);
   });
 });
